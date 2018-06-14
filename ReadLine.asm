30ccc742398cd080896b0534133b7a8e08c28c8e
photon1592 <photon1592@gmail.com>
2018-06-14 19:02:22 +0900

    comment, 파일핸들은 ebx로 통일

--- ReadLine.asm
+++ ReadLine.asm
@@ -1,13 +1,20 @@
 include Irvine32.inc
 
 ; 파일을 한줄씩 읽는다
-;input-eax:filehandle의 값을 가진다
+;input-ebx:filehandle의 값을 가진다
 ;output- eax-현재온도(파일끝: -1)
 ;        ebx-목표온도(자동모드일 경우 -1)
 ;중요:미리 매인에서 createFile을 호출해 filehandle값을 가지고 있어야한다
 ;
 
+;공백을 제외하고 글자 하나를 입력받음
+;ebx in: file handle
+;eax out: 입력받은 글자의 zero extension, 실패(또는 파일끝)시 -1
 file_read_char proto
+
+;소숫점을 무시하고 양수를 입력받음, 앞쪽 공백 무시
+;ebx in: file handle
+;eax out: 입력받은 수, 실패시 0
 file_read_int_ignore_point proto
 
 .data
@@ -18,43 +25,43 @@ ReadLine proc
 	push ecx
 	push edx
 
-	mov ebx, eax
-
 _start:
 
-	;첫 글자
+	;첫 글자 파일로부터 읽기 ->eax
 	call file_read_char
 
 	cmp eax, 'm'
-	je _manual
+	je _manual ;m은 manual
 	cmp eax, 'a'
-	je _automatic
+	je _automatic ;a는 auto
 
-	mov eax, -1;EOF or error
+	mov eax, -1 ;파일 끝이거나 오류, eax = -1로 이를 표시함
 	jmp _END
 
 _manual:
 
 	;현재 온도
 	call file_read_int_ignore_point
-	mov ecx, eax
+	mov ecx, eax ; ecx: 현재온도 * 10
 
 	;목표온도
 	call file_read_int_ignore_point
 	mov edx, 10
-	mul edx
+	mul edx ; eax: 목표온도 * 10
 
-	mov ebx, eax
-	mov eax, ecx
+	mov ebx, eax ; ebx: 목표온도 * 10
+	mov eax, ecx ; eax: 현재온도 * 10
 	jmp _END
 
 _automatic:
 
 	;현재 온도
 	call file_read_int_ignore_point
+	;eax = 현재온도 * 10
 
 	;나중 온도
 	mov ebx, -1
+	;ebx = auto
 
 _END:
 
--- main2.asm
+++ main2.asm
@@ -1,8 +1,11 @@
 include Irvine32.inc
 
 ReadLine proto
-WriteLine proto
+;WriteLine proto
+
+;eax의 unsigned int 값을 file handle ebx에 출력, 성공시 eax에 1, 실패시 0을 리턴
 file_writeln_int proto
+
 manual proto
 auto proto
 
@@ -19,29 +22,30 @@ main2 proc
 	;add esp, -8
 	push eax
 	push ebx
-	push ecx
-	push edx
+	push ecx; CreateFile에서 eax, ecx, edx를 수정함
+	push edx; "
 	push esi
 	push edi
 	
     INVOKE CreateFile, ADDR infilename, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0
-	mov esi, eax
-	cmp eax, -1
+	mov esi, eax ; esi: input.txt
+	cmp eax, INVALID_HANDLE_VALUE
 	je ERROR1
 
     INVOKE createFile, ADDR outfilename, GENERIC_WRITE, FILE_SHARE_READ, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0
-    mov edi, eax
-	cmp eax, -1
-	je ERROR2
+    mov edi, eax ; edi: output.txt
+	cmp eax, INVALID_HANDLE_VALUE
+	je ERROR2 ; esi를 닫고 에러메시지 출력
 
 _READ:
-    mov eax, esi
+    mov ebx, esi
     call ReadLine
 
 	cmp eax, -1
-	je _ENDREAD
+	je _ENDREAD ; eax = -1 -> 끝
+
 	cmp ebx, -1
-	je _AUTO
+	je _AUTO ; ebx = -1 -> auto
 
 	call manual
 	jmp _WRITE
@@ -49,9 +53,12 @@ _AUTO:
 	call auto
 
 _WRITE:
+
+	; eax = auto 또는 manual의 결과, 설정된 또는 계산된 목표온도
     mov ebx,edi
     call file_writeln_int
-	jmp _READ
+	;출력오류는 체크하지 않음
+	jmp _READ ; 다음 줄 반복
 
 _ENDREAD:
     INVOKE CloseHandle, esi
